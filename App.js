6932f8ee2c7a9af5eb86451e7e3c7a6e28fb3933
suryaprakash <surya.zvky@gmail.com>
2020-11-26 23:14:02 +0530

    added the numbers and made usage of flexbox

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import {ScrollView, Text, View, Image} from 'react-native';
+import {
+  ScrollView,
+  Text,
+  View,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 import Sound from 'react-native-sound';
 
 const App = () => {
@@ -17,13 +24,50 @@ const App = () => {
   ];
 
   return (
-    <ScrollView>
-      <View>
-        <Image source={require('./assets/logo.png')} />
-        <Text>Spanish Number</Text>
+    <ScrollView style={styles.container}>
+      <Image style={styles.logo} source={require('./assets/logo.png')} />
+      <View style={styles.gridContainer}>
+        {soundList.map((sound) => (
+          <TouchableOpacity style={styles.box} key={sound}>
+            <Text style={styles.text}>{sound}</Text>
+          </TouchableOpacity>
+        ))}
       </View>
     </ScrollView>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1b262c',
+  },
+  logo: {
+    alignSelf: 'center',
+    marginTop: 15,
+  },
+  gridContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'flex-start',
+    justifyContent: 'space-around',
+  },
+  box: {
+    height: 110,
+    backgroundColor: '#0f4c75',
+    width: '46%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 15,
+    borderRadius: 5,
+    shadowColor: '#393e46',
+    elevation: 5,
+  },
+  text: {
+    fontSize: 50,
+    color: '#ff4301',
+  },
+});
